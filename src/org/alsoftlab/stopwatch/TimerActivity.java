e17562f358d681d6e617f92988e8aaae8cd1ff78
Alex Busenius <alex.busenius@googlemail.com>
2014-01-19 21:17:49 +0100

    Trying out things

--- src/org/alsoftlab/stopwatch/TimerActivity.java
+++ src/org/alsoftlab/stopwatch/TimerActivity.java
@@ -1,15 +1,25 @@
 package org.alsoftlab.stopwatch;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class TimerActivity extends Activity {
+    private boolean mStarted = false;
+    private long mStartTime = 0;
+    private Chronometer mChronometer;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timer_activity);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer1);
+        mText = (TextView) findViewById(R.id.textView1);
     }
 
     @Override
@@ -19,4 +29,21 @@ public class TimerActivity extends Activity {
         return true;
     }
 
+    public void button_OnClick(View view) {
+        // assert (view.getId() == R.id.imageButton1);
+
+        if (mStarted) {
+            mChronometer.stop();
+            mStarted = false;
+
+            long diff = SystemClock.elapsedRealtime() - mStartTime;
+            mText.setText(String.format("%03d", diff % 1000));
+        } else {
+            mStartTime = SystemClock.elapsedRealtime();
+            mChronometer.setBase(mStartTime);
+            mChronometer.start();
+            mStarted = true;
+            mText.setText("000");
+        }
+    }
 }
